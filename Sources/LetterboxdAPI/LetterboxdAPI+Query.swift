ab83549dc74f73d1f1b084d60923ba38faa0e2f5
Gianpiero <gs@grape.io>
2021-10-05 09:53:53 +0200

    Added a `query` function for not yet supported endpoints

---
+++ Sources/LetterboxdAPI/LetterboxdAPI+Query.swift
@@ -0,0 +1,37 @@
+//
+//  LetterboxdAPI+Query.swift
+//  LetterboxdAPI
+//
+//  Created by Gianpiero Spinelli.
+//
+
+import Foundation
+
+public extension LetterboxdAPI {
+    /// Generic query for any endpoint on the letterboxd APIs
+    /// - Parameters:
+    ///   - path: the endpoint, eg: `/film/id`
+    ///   - parameters: the parameters for the endpoint
+    ///   - body: if the endpoint supports a body, please insert use it here
+    ///   - completion: the completion of the request
+    func query<D: Decodable>(path: String, parameters: [String: String], body: String? = nil, completion: @escaping (Result<D, Error>) -> Void) {
+        query(path: path, parameters: parameters, body: body?.data(using: .utf8), completion: completion)
+    }
+    
+    /// Generic query for any endpoint on the letterboxd APIs
+    /// - Parameters:
+    ///   - path: the endpoint, eg: `/film/id`
+    ///   - parameters: the parameters for the endpoint
+    ///   - body: if the endpoint supports a body, please insert use it here
+    ///   - completion: the completion of the request
+    func query<D: Decodable>(path: String, parameters: [String: String], body: Data? = nil, completion: @escaping (Result<D, Error>) -> Void) {
+        let url = URLBuilder.url(path: path, body: body, params: parameters)
+        
+        guard let request = generateRequest(url: url, method: .get) else {
+            completion(.failure(LetterboxdAPIError.generatingRequest))
+            return
+        }
+        
+        processRequest(request: request, completion: completion)
+    }
+}
