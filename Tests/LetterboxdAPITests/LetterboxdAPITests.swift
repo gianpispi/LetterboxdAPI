cdc3ce2965eedf56316f70505396999637f171c7
Gianpiero Spinelli <gianpispi@me.com>
2024-06-23 00:33:11 +0200

    Add test

--- Tests/LetterboxdAPITests/LetterboxdAPITests.swift
+++ Tests/LetterboxdAPITests/LetterboxdAPITests.swift
@@ -2,9 +2,9 @@
 import XCTest
 
 final class LetterboxdAPITests: XCTestCase {
-  func testAPIKeysSetup() {
-    LetterboxdAPI.setUpAPIKeys(publicAPI: "test", privateAPI: "test")
-    XCTAssertNotEqual(Private.privateAPIKey, "")
-    XCTAssertNotEqual(Private.publicAPIKey, "")
+  func testURLBuilder() {
+    let request = Path("/auth/token").generateRequest(withMethod: .post)
+    XCTAssertEqual(request.httpMethod, "POST")
+    XCTAssertEqual(request.url?.absoluteString, "https://api.letterboxd.com/api/v0/auth/token")
   }
 }
